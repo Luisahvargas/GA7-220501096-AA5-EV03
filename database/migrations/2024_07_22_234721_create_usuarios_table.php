f5d936fa78560d89009de2b1b5ade555bbf7e9af
Luisahvargas <99419147+Luisahvargas@users.noreply.github.com>
2024-07-22 21:55:52 -0500

    Add files via upload

---
+++ database/migrations/2024_07_22_234721_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Nombre');
+            $table->string('Apellido');
+            $table->integer('Cedula');
+            $table->string('Email');
+            $table->string('Cargo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
